25608449efa0585f9da7150529dd1c8a967b3c65
leeeeeoy <hoheho18@naver.com>
2026-02-22 19:25:23 +0900

    feat: env 파일 제거

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:better_way/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv
 
-Future<void> main() async { // Make main() async
+void main() {
   usePathUrlStrategy();
 
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env"); // Load .env file
 
   runApp(ProviderScope(child: App()));
 }
